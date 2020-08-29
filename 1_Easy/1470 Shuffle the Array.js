561dcc94b47ddbc7d95a603f7caaa1fea2c7bcee
365kim <60066472+365kim@users.noreply.github.com>
2020-08-29 22:57:04 +0900

    2020. 08. 29. Solve 1470

---
+++ 1_Easy/1470 Shuffle the Array.js
@@ -0,0 +1,12 @@
+/* https://leetcode.com/problems/shuffle-the-array/ */
+
+var shuffle = function(nums, n) {
+  
+  let result = [];
+  
+  for ( let i = 0; i < n; i++ ) {
+    result.push(nums[i]);
+    result.push(nums[n + i]);
+  }
+  return (result);
+};
