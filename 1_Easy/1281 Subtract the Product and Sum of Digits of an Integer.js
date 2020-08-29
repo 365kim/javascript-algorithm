799a9d5ae25b472fd0f1519ae4c87c89f24798c1
365kim <60066472+365kim@users.noreply.github.com>
2020-08-29 23:16:55 +0900

    2020. 08. 29. Solve 1281

---
+++ 1_Easy/1281 Subtract the Product and Sum of Digits of an Integer.js
@@ -0,0 +1,14 @@
+/* https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/ */
+
+var subtractProductAndSum = function(n) {
+  let array = n.toString().split('');
+  let product = 1;
+  let sum = 0;
+  
+  for (i of array) {
+    product = product * i;
+    sum = sum + (+i); // convert to Number to avoid String operation
+  };
+  
+  return ( product - sum );
+};
