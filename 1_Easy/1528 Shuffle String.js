b544d0030d9cc925f76152e9974cd0a264fa39f9
365kim <60066472+365kim@users.noreply.github.com>
2020-08-29 23:06:37 +0900

    2020. 08. 29. Solve 1528

---
+++ 1_Easy/1528 Shuffle String.js
@@ -0,0 +1,9 @@
+/* https://leetcode.com/problems/shuffle-string/ */
+
+var restoreString = function(s, indices) {
+  let array = [];
+  for (i of indices) {
+    array[indices[i]] = s[i];
+  }
+  return (array.join(''));
+};
