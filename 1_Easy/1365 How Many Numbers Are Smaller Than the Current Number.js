449a2ecaae0932614db4cdcd6f93041c95bf0b93
365kim <60066472+365kim@users.noreply.github.com>
2020-08-30 20:03:54 +0900

    2020. 08. 30. Solve 1365

---
+++ 1_Easy/1365 How Many Numbers Are Smaller Than the Current Number.js
@@ -0,0 +1,12 @@
+/* https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/ */
+
+var smallerNumbersThanCurrent = function(nums) {
+  let sortedNums = nums.slice().sort((a, b) => a - b);
+  let result = [];
+  
+  for ( let num of nums ) {
+    result.push(sortedNums.indexOf(num));
+  }
+  
+  return (result);
+};
