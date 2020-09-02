df3c084c915b2d7598ff709c4851375ff5e0e5de
365kim <365listener@gmail.com>
2020-09-02 15:43:39 +0900

    2020. 09. 02. Solve 771

---
+++ 1_Easy/0771 Jewels and Stones.js
@@ -0,0 +1,13 @@
+/* https://leetcode.com/problems/jewels-and-stones/ */
+
+var numJewelsInStones = function(J, S) {
+    let count = 0;
+    for (stone of S) {
+        if (J.indexOf(stone) != -1) {
+            count++;
+        }
+    }
+    return count;
+};
+
+console.log(numJewelsInStones("aA", "aAAbbbb"));
