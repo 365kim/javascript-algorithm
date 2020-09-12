536f6b704b03003a473774a9ce100c85dba1b727
365kim <365listener@gmail.com>
2020-09-12 13:29:26 +0900

    2020. 09. 12. Solve 1

---
+++ 1_Easy/0001 Two Sum.js
@@ -0,0 +1,13 @@
+/* https://leetcode.com/problems/two-sum/ */
+
+let twoSum = (nums, target) => {
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+console.log(twoSum((nums = [2, 7, 11, 15]), (target = 9)));
