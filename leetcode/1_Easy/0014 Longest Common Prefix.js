1d689331958bc454c904f07d127c4b6b719f625e
365kim <365listener@gmail.com>
2021-01-21 12:00:39 +0900

    2021. 01. 21. Solve

---
+++ leetcode/1_Easy/0014 Longest Common Prefix.js
@@ -0,0 +1,6 @@
+/* https://leetcode.com/problems/longest-common-prefix/ */
+
+console.log(longestCommonPrefix((strs = ['flower', 'flow', 'flight'])));
+console.log(longestCommonPrefix((strs = ['dog', 'racecar', 'car'])));
+console.log(longestCommonPrefix((strs = ['ab', 'a'])));
+console.log(longestCommonPrefix((strs = [])));
