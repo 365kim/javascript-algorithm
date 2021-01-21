0f03a6153487bcb94608c021a6112d075b56e8bf
365kim <365listener@gmail.com>
2021-01-21 12:47:40 +0900

    2021. 01. 21. Solve

---
+++ leetcode/1_Easy/0344 Reverse String.js
@@ -0,0 +1,18 @@
+/* */
+
+var reverseString = function (s) {
+  //  return s.reverse();
+
+  let iterationNum = Math.floor(s.length / 2);
+
+  for (let i = 0; i < iterationNum; i++) {
+    let temp = s[i];
+
+    s[i] = s[s.length - 1 - i];
+    s[s.length - 1 - i] = temp;
+  }
+  return s;
+};
+
+console.log(reverseString((Input = ['h', 'e', 'l', 'l', 'o'])));
+console.log(reverseString((Input = ['H', 'a', 'n', 'n', 'a', 'h'])));
