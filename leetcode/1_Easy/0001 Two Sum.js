96f3b33ccfcc99e05e20d64a28ea4b9757518ab8
365kim <365listener@gmail.com>
2022-05-29 16:58:17 +0900

    2022. 05. 29. Solve

--- leetcode/1_Easy/0001 Two Sum.js
+++ leetcode/1_Easy/0001 Two Sum.js
@@ -12,3 +12,19 @@ const twoSum = (nums, target) => {
 
 console.log(twoSum((nums = [2, 7, 11, 15]), (target = 9))); // [0, 1]
 console.log(twoSum((nums = [2, 7, 11, 15]), (target = 9))); // [0, 1]
+
+// O(n)
+function twoSum2(nums, target) {
+  const map = new Map(); // <value, index>
+
+  for (let i = 0; i < nums.length; i++) {
+    const remainder = target - nums[i];
+
+    if (map.has(remainder)) {
+      const j = map.get(remainder);
+      return [j, i];
+    }
+
+    map.set(nums[i], i);
+  }
+}
