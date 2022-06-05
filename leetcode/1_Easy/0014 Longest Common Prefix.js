4d6ea2403d4bb4ab3f703f6f54c3db1a571a4192
365kim <365listener@gmail.com>
2022-06-05 20:17:28 +0900

    2022. 06. 05. Solve

--- leetcode/1_Easy/0014 Longest Common Prefix.js
+++ leetcode/1_Easy/0014 Longest Common Prefix.js
@@ -1,6 +1,32 @@
 /* https://leetcode.com/problems/longest-common-prefix/ */
 
-console.log(longestCommonPrefix((strs = ['flower', 'flow', 'flight'])));
-console.log(longestCommonPrefix((strs = ['dog', 'racecar', 'car'])));
-console.log(longestCommonPrefix((strs = ['ab', 'a'])));
-console.log(longestCommonPrefix((strs = [])));
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefix = (strs) => {
+  const str = strs[0];
+  let result = '';
+
+  for (let n = 0; n < str.length; n++) {
+    const char = str[n];
+
+    if (char === undefined) {
+      return result;
+    }
+
+    for (let j = 1; j < strs.length; j++) {
+      if (char !== strs[j][n]) {
+        return result;
+      }
+    }
+    result += char;
+    console.log(result);
+  }
+  return result;
+};
+
+console.log(longestCommonPrefix((strs = ['flower', 'flow', 'flight']))); // "fl"
+console.log(longestCommonPrefix((strs = ['dog', 'racecar', 'car']))); // ""
+console.log(longestCommonPrefix((strs = ['flower', 'flower', 'flower', 'flower']))); // "flower"
+console.log(longestCommonPrefix((strs = []))); // ""
