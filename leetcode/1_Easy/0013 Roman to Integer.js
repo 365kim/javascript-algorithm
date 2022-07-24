6bad760b607aae8a67af366b10f6c4599c2226d9
365kim <365listener@gmail.com>
2022-07-24 09:16:31 +0900

    2022. 07. 24. Solve

---
+++ leetcode/1_Easy/0013 Roman to Integer.js
@@ -0,0 +1,43 @@
+const map = {
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000,
+};
+
+const romanToInt = (s) => {
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (i + 1 < s.length) {
+      const twoDigits = s[i] + s[i + 1];
+      const twoDigitsToInt = map[twoDigits];
+
+      if (twoDigitsToInt) {
+        result += twoDigitsToInt;
+        i++;
+        continue;
+      }
+    }
+
+    const oneDigit = s[i];
+    const oneDigitToInt = map[oneDigit];
+    console.log('+', oneDigitToInt);
+    result += oneDigitToInt;
+  }
+
+  return result;
+};
+
+console.log(romanToInt('III')); // 3
+console.log(romanToInt('LVIII')); // 58
+console.log(romanToInt('MCMXCIV')); // 1994
