d5124138627e8aa2c8368d2efcecba1eebe8c259
365kim <365listener@gmail.com>
2022-07-24 09:22:51 +0900

    2022. 07. 24. Solve

---
+++ leetcode/1_Easy/0326 Power of Three.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * -231 <= n <= 231 - 1
+
+ * @return {boolean}
+ */
+const isPowerOfThree = (n) => {
+  if (n <= 0) return false;
+
+  while (n > 1) {
+    n /= 3;
+  }
+  while (n < 1) {
+    n *= 3;
+  }
+
+  return n === 1;
+};
+
+console.log(isPowerOfThree(27));
+console.log(isPowerOfThree(0));
+console.log(isPowerOfThree(9));
