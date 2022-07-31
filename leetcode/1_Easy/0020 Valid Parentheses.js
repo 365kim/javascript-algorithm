22a6508a956b2705047890a5d32572a5513c2238
365kim <365listener@gmail.com>
2022-07-31 14:41:30 +0900

    2022. 07. 31. Solve

---
+++ leetcode/1_Easy/0020 Valid Parentheses.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+const isValid = function (s) {
+  const STARTS = ['(', '[', '{'];
+  let stack = [];
+  const top = () => stack[stack.length - 1];
+
+  for (let char of s) {
+    if (STARTS.includes(char)) {
+      stack.push(char);
+      continue;
+    }
+    if (stack.length === 0) return false;
+
+    if (char === ')') {
+      if (top() !== '(') return false;
+      stack.pop();
+      continue;
+    }
+    if (char === ']') {
+      if (top() !== '[') return false;
+      stack.pop();
+      continue;
+    }
+    if (char === '}') {
+      if (top() !== '{') return false;
+      stack.pop();
+    }
+  }
+
+  if (stack.length > 0) return false;
+
+  return true;
+};
+
+console.log(isValid((s = '()')));
+console.log(isValid((s = '()[]{}')));
