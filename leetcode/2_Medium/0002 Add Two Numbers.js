f4dd6399c6946b481423c29fb9dd401311a802f0
365kim <365listener@gmail.com>
2022-08-21 21:49:44 +0900

    2022. 08. 21. Solve

--- leetcode/2_Medium/0002 Add Two Numbers.js
+++ leetcode/2_Medium/0002 Add Two Numbers.js
@@ -1,33 +1,4 @@
 /* https://leetcode.com/problems/add-two-numbers/ */
-
-let addTwoNumbers = (list1, list2) => {
-  let root = new ListNode();
-  let curr = root;
-
-  let [num1, num2, sum] = ["", "", 0];
-
-  while (list1) {
-    num1 += list1.val;
-    list1 = list1.next;
-  }
-  while (list2) {
-    num2 += list2.val;
-    list2 = list2.next;
-  }
-
-  sum =
-    BigInt([...num1].reverse().join("")) + BigInt([...num2].reverse().join(""));
-  sum = [...sum.toString()].reverse();
-
-  for (let i = 0; i < sum.length - 1; i++) {
-    curr.val = sum[i];
-    curr.next = new ListNode();
-    curr = curr.next;
-  }
-  curr.val = sum[sum.length - 1];
-  return root;
-};
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -35,3 +6,30 @@ let addTwoNumbers = (list1, list2) => {
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
+const addTwoNumbers = (list1, list2) => {
+  let root = new ListNode(-1);
+  let cur = root;
+  let carry = 0;
+
+  while (list1 || list2 || carry !== 0) {
+    const val1 = list1?.val ?? 0;
+    const val2 = list2?.val ?? 0;
+    let sum = val1 + val2 + carry;
+
+    if (sum >= 10) {
+      carry = 1;
+      sum -= 10;
+    } else {
+      carry = 0;
+    }
+
+    cur.next = new ListNode(sum);
+    cur = cur.next;
+
+    if (list1) list1 = list1.next;
+    if (list2) list2 = list2.next;
+  }
+
+  return root.next;
+};
