1abeff50cd6c71bd6d878731980575ee0f7d518f
365kim <365listener@gmail.com>
2023-01-30 11:28:09 +0900

    2023. 01. 30. Solve

---
+++ leetcode/2_Medium/0150. Evaluate Reverse Polish Notation.js
@@ -0,0 +1,55 @@
+const operate = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b > 0 ? Math.floor(a / b) : Math.ceil(a / b);
+  }
+};
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+
+const evalRPN = (tokens) => {
+  const getValidNumberTokens = (cur) => {
+    let backwardIndex = cur;
+    const answer = [];
+
+    while (backwardIndex >= 0 && answer.length !== 2) {
+      const char = tokens[backwardIndex];
+      const num = Number(char);
+      const visited = char === null;
+      if (!visited && !Number.isNaN(num)) {
+        answer.push(num);
+        tokens[backwardIndex] = null;
+      }
+      backwardIndex--;
+    }
+    return answer;
+  };
+
+  let i = 0;
+
+  while (i < tokens.length) {
+    const char = tokens[i];
+
+    if (!Number.isNaN(Number(char))) {
+      i++;
+      continue;
+    }
+
+    const [a, b] = getValidNumberTokens(i);
+    const result = operate(b, a, char);
+
+    tokens[i] = result;
+    i++;
+  }
+
+  return tokens[tokens.length - 1];
+};
