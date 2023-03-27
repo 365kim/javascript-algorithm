e861066b88d79b9d537e3f81928e9e7c6fb574a0
365kim <365listener@gmail.com>
2023-03-27 10:36:36 +0900

    2023. 03. 27. Solve

---
+++ leetcode/2_Medium/0046 Permutations.js
@@ -0,0 +1,30 @@
+/*
+ * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ * n개의 수 중 k개를 고르는 순열 경우의 수: n! / (n-k)!
+ *
+ * - 1 <= nums.length <= 6
+ * - -10 <= nums[i] <= 10
+ * - All the integers of nums are unique.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = (nums) => {
+  const answer = [];
+
+  recursive([], nums, answer, nums.length);
+  return answer;
+};
+
+function recursive(cur, rest, answer, targetLength) {
+  if (cur.length === targetLength) {
+    answer.push(cur);
+    return;
+  }
+  rest.forEach((n, i) => recursive([...cur, n], [...rest.slice(0, i), ...rest.slice(i + 1)], answer, targetLength));
+}
+
+console.log(permute((nums = [1, 2, 3])));
+// [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
