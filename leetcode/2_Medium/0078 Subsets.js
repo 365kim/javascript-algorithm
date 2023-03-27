4ed7265c9739d244e08c921fac6705238d8b0be6
365kim <365listener@gmail.com>
2023-03-27 11:06:05 +0900

    2023. 03. 27. Solve

--- leetcode/2_Medium/0078 Subsets.js
+++ leetcode/2_Medium/0078 Subsets.js
@@ -1,6 +1,5 @@
 /*
- * Given an integer array nums of unique elements, return all possible 
-subsets (the power set).
+ * Given an integer array nums of unique elements, return all possible subsets (the power set).
  * - The solution set must not contain duplicate subsets. Return the solution in any order.
  * - 1 <= nums.length <= 10
  * - -10 <= nums[i] <= 10
