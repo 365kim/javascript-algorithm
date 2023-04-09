31882b4c40cb13c74a3d363e3f4b7b423dbabb89
365kim <365listener@gmail.com>
2023-04-09 22:44:14 +0900

    2023. 04. 09. Solve

--- leetcode/1_Easy/0704 Binary Search.js
+++ leetcode/1_Easy/0704 Binary Search.js
@@ -1,29 +1,29 @@
 /* https://leetcode.com/problems/binary-search/ */
 
-var search = function (nums, target) {
-  let front = 0;
-  let end = nums.length - 1;
-  let idx = Math.floor((front + end) / 2);
+const search = (nums, target) => {
+  let s = 0
+  let e = nums.length - 1
 
-  while (end - front > 1) {
-    if (nums[idx] < target) {
-      front = idx;
-    } else {
-      end = idx;
+  while (s <= e) {
+    const mid = Math.floor((s + e) / 2)
+
+    if (nums[mid] === target) {
+      return mid
     }
-    idx = Math.floor((front + end) / 2);
-  }
-  if (nums[front] === target) {
-    return front;
-  }
-  if (nums[end] === target) {
-    return end;
-  }
-  return -1;
-};
 
-console.log(search((nums = [-1, 0, 3, 5, 9, 12]), (target = 9)));
-console.log(search((nums = [-1, 0, 3, 5, 9, 12]), (target = 2)));
-console.log(search((nums = [-1]), (target = 2)));
-console.log(search((nums = [-1]), (target = -1)));
-console.log(search((nums = [-1]), (target = -3)));
+    if (nums[mid] < target) {
+      s = mid + 1
+    } else {
+      e = mid - 1
+    }
+  }
+
+  return -1
+}
+
+console.log(search((nums = [-1, 0, 3, 5, 9, 12]), (target = 9)))
+console.log(search((nums = [-1, 0, 3, 5, 9, 12]), (target = 2)))
+console.log(search((nums = [-1, 0, 3, 5, 9, 12]), (target = 12)))
+console.log(search((nums = [-1]), (target = 2)))
+console.log(search((nums = [-1]), (target = -1)))
+console.log(search((nums = [-1]), (target = -3)))
